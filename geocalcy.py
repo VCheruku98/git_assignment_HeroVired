62f1ae229c5b7c3b1b1b1150e36aff6d8eb60f07
vcheruku <cherukuvivek98@gmail.comm>
2024-02-02 22:45:57 +0530

    rectangle_area_implemented

--- geocalcy.py
+++ geocalcy.py
@@ -13,3 +13,11 @@ return length * width
 if __name__ == "__main__":
 
 calculator = GeometryCalculator()
+
+# Vivek has successfully Implemented the feature to calculate the area of a rectangle 
+
+length = 10
+
+width = 6
+
+print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
